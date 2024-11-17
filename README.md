0e4fe11046d50e08e1fdfbd647a156f0b0a4956a
Patrick Holmes <patrick.j.holmes@gmail.com>
2024-11-17 11:20:21 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Really simple calls to Bluesky API
+
+Steps
+
+1. npm install
+
+2. export BLUESKY_PASSWORD="mySecretPassword"
+
+3. tsc
+
+4. node ./dist/index.js
---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import { AtpAgent } from '@atproto/api'
+
+const agent = new AtpAgent({
+  service: 'https://bsky.social'
+})
+
+await agent.login({
+  identifier: 'patrick.j.holmes@gmail.com',
+  password: process.env.BLUESKY_PASSWORD
+})
+
+// post a message
+
+// await agent.post({
+//   text: 'Hello world! I posted this via the API.',
+//   createdAt: new Date().toISOString()
+// })
+
+// get the first page of the timeline
+
+// const { data } = await agent.getTimeline({
+//   cursor: "",
+//   limit: 30,
+// });
+// const { feed: postsArray, cursor: nextPage } = data;
+// console.log(JSON.stringify(postsArray, null, 2));
+
+// get a user's profile
+
+const { data } = await agent.getProfile({ actor: 'did:plc:5k7u2ygbcigv2v62nawsuxsa' })
+console.log(JSON.stringify(data, null, 2))
+
+// the end
+console.log('--end--')
